16585a9eba08d953dbbbc69047597cac6f215662
Bukurie Hysa <147159931+BukurieH@users.noreply.github.com>
2023-11-21 15:45:01 +0100

    Add files via upload

---
+++ quizgamepython.py
@@ -0,0 +1,28 @@
+print("Welcome to my computer quiz!")
+
+playing=input("Do you want to play? ")
+
+print(playing)
+
+if playing.lower() != "yes":
+    quit()
+    
+print("Okay! LEt's play :)")
+score = 0
+
+answer = input("What does CPU stand for? ")
+if answer.lower() == "central processing unit":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("What does GPU stand for? ")
+if answer.lower() == " graphics processing unit":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+print("You got " +  str(score) + " questions correct!")  
+print("You got " +  str((score/2)* 100 ) + "%")  
